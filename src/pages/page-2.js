1e0db5fa898a8b3a6a770d66dd958ce28025ac74
albert <57201727+albertmayer1996@users.noreply.github.com>
2019-10-31 10:37:57 +0700

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -7,8 +7,17 @@ import SEO from "../components/seo"
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>Biodata</h1>
+    <p>Nama : Albert Reinhold Mayernest</p>
+    <p>Tempat tanggal lahir : Samarinda, 10 Mei 1996</p>
+    <p>Alamat : Villa Tajur Blok. A1, no. 01, Bogor</p>
+    <p>WHatsapp : 081224400250</p>
+    <p>Facebook : Albert Mayer</p>
+    <p>Instagram : Albertmayerr</p>
+    <p>Cita-cita : Kurus</p>
+    <p>Hobby : Makan dan tidur</p>
+    <p>Kegiatan sehari-hari : Ngampus, push rank, gitskuy</p>
+    <p>Kata-kata motivasi : "Jadilah dirimu sendiri, orang lain tidak akan merasakan hartamu dimasa mendatang"</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
